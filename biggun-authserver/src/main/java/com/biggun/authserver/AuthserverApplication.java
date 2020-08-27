780442577be556e2b4cbea784ecc15857261a9b2
biggun <biggun@tec.com>
2020-08-27 19:03:29 +0800

    auth2代码练习

---
+++ biggun-authserver/src/main/java/com/biggun/authserver/AuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.biggun.authserver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by biggun on 2020/8/27
+ */
+@SpringBootApplication
+public class AuthserverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthserverApplication.class,args);
+    }
+}
---
+++ biggun-authserver/src/main/java/com/biggun/authserver/config/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.biggun.authserver.config;
+
+/**
+ * Created by biggun on 2020/8/27
+ */
+public class WebSecurityConfig {
+}
